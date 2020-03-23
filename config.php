25a42839cbd2e73555542c9b512514c9cb07af0e
John Teo <john.teo.2018@sis.smu.edu.sg>
2020-03-23 23:45:39 +0800

    check

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+## TelegramBot info
+define('BOT_TOKEN', '931840409:AAFURs_LuoVkTDXeLy0AmLLecG2IhYjpjUM');
+define('BOT_USERNAME', 'G4T3MS_bot');
+
+## You can use you account or channel or gtoup for geting notification.
+// Get you chat_id from @FalconGate_Bot
+define('CHAT_ID', 173840098);
+
+// if you want get only error state switch this to 'false'
+define('ANY_STATE', true);
+
+## If you do not neet get notify for some of your monitoring website, You can add your site name in this array.
+// You must use your monitoring website name.
+$exceptionList = array();
